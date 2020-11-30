dec4a0accbbcf75e3749a54898befae940a0964d
Gina Sprint <sprint@gonzaga.edu>
2020-11-30 11:16:34 -0800

    Initial Commit

---
+++ SwiftUIFunS2/SwiftUIFunS2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFunS2App.swift
+//  SwiftUIFunS2
+//
+//  Created by Gina Sprint on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFunS2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
