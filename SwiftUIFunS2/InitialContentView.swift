1383dd036215659c2b3c40e475a6db1c6ef2640c
Gina Sprint <sprint@gonzaga.edu>
2020-12-08 09:37:29 -0800

    Add custom modifier to change navigation bar background color to systemTeal

--- SwiftUIFunS2/InitialContentView.swift
+++ SwiftUIFunS2/InitialContentView.swift
@@ -22,6 +22,7 @@ struct InitialContentView: View {
             })
             .padding()
         }
+        .navigationBarColor(.systemTeal) // see NavigationBarModifier.swift for implementation
     }
 }
 
